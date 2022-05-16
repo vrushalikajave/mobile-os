f46a2becad93f74e51ce97e6bc3710d5a6cc5166
vrkajave1994 <99703905+vrushalikajave@users.noreply.github.com>
2022-05-16 20:26:05 +0530

    Updated
    Updated

---
+++ src/components/Os.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Os = () => {
+  return (
+    <div>
+      <h1>Mobile Operating System</h1>
+      <ul>
+          <li>Android</li>
+          <li>Blackberry</li>
+          <li>iPhone</li>
+          <li>Windows Phones</li>
+      </ul>
+      <h1>Mobile Manufacturers</h1>
+      <ul>
+          <li>Samsung</li>
+          <li>HTC</li>
+          <li>Micromax</li>
+          <li>Apple</li>
+      </ul>
+      
+    </div>
+  )
+}
+
+export default Os
