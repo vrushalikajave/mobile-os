b178161ca1144f1dd1c128dfa9397973c0f0b12d
vrkajave1994 <99703905+vrushalikajave@users.noreply.github.com>
2022-06-08 08:53:51 +0530

    Style added

--- src/components/Os.jsx
+++ src/components/Os.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
+import styles from "./Os.module.css"
 
 const Os = () => {
   return (
-    <div>
+    <div >
       <h1>Mobile Operating System</h1>
-      <ul>
+      <ul className={styles.mainDiv}>
           <li>Android</li>
           <li>Blackberry</li>
           <li>iPhone</li>
           <li>Windows Phones</li>
       </ul>
       <h1>Mobile Manufacturers</h1>
-      <ul>
+      <ul className={styles.mainDiv}>
           <li>Samsung</li>
           <li>HTC</li>
           <li>Micromax</li>
